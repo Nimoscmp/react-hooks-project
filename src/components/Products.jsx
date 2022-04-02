0cecf26de12b3909210fcc44736ec287df5b5fb2
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-04-02 08:35:54 -0500

    Modify Products and Checkout

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,14 +4,14 @@ import Product from './Product'
 function Products({ products }) {
   return (
     <section className="Products">
-      <div className="Products-items">
+      <ul className="Products-items">
         {products.map(product => (
           <Product 
             key={product.id}
             product={product}
           />
         ))}
-      </div>
+      </ul>
     </section>
   )
 }
--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Checkout() {
   return (
-    <h1>Checkout</h1>
+    <section className="Checkout">
+      <ul className="Checkout-content">
+        <h3>Lista de pedidos</h3>
+        <div className="Checkout-element">
+          <h4>ITEM name</h4>
+          <span>$10</span>
+        </div>
+        <button type="button">Eliminar</button>
+      </ul>
+      <div className="Checkout-sidebar">
+        <h3>Precio total: $100</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
+    </section>
   )
 }
 
